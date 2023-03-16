e5acf8041dcf9a4d063e4b6b0405aedb24bb7433
Alberto vargas <alberto.vargas2104@gmail.com>
2023-03-16 17:53:32 -0300

    Primer commit estable

---
+++ AppCoder/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Creacion de modelo del proyecto Curso
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
+# Creacion de modelo del curso
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f"Curso: {self.nombre}, Camada: {self.camada}"
+    
+# Creacion de modelo, estudiantes
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Profesor: {self.nombre}, {self.apellido}"
+    
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()
+
+
+
+
+
